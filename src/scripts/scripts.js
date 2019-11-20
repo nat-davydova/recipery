011653c13295a14b2463dc0b0f9a483a9331923e
nat-davydova <natti.davydova@gmail.com>
2019-11-20 18:57:58 +0300

    Basic JS preps

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,1 +1,22 @@
+//APP CONTROLLER
 
+//import models
+
+//import views
+
+//import utils and configs
+import { DOM } from './modules/configs/path';
+
+//state
+//here stored:
+//- search query
+//- faved recipes
+const state = {
+
+};
+
+//*** SEARCH HANDLER
+
+const searchController = async () => {
+
+};
