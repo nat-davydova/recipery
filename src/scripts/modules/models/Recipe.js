56117f10aef706ee558af99212e1845cb14a3eb0
nat-davydova <natti.davydova@gmail.com>
2020-01-08 18:21:00 +0300

    Recipe: get recipe info from server

--- src/scripts/modules/models/Recipe.js
+++ src/scripts/modules/models/Recipe.js
@@ -1,12 +1,27 @@
+import axios from 'axios';
+import { recipeApi } from "../configs/apiKeys";
+
 export default class Recipe {
 
 	constructor(id) {
 		this.id = id;
 	}
 
-	grabFullRecipe = () => {
+	grabFullRecipe = async () => {
 
-		console.log('grab!');
+		try {
+			const apiURL = `https://api.spoonacular.com/recipes/`;
+			const apiKey = `apiKey=${recipeApi}`;
+			const recipeQuery = `${this.id}/information`;
+
+			const fullRecipe = await axios(`${apiURL}${recipeQuery}/?${apiKey}`);
+
+			console.log(`${apiURL}${recipeQuery}/?${apiKey}`);
+			console.log(fullRecipe);
+
+		} catch (e) {
+
+		}
 
 	};
 
