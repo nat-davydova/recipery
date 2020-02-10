d7e4f03e75b9b492381136f29778abc159288e69
nat-davydova <natti.davydova@gmail.com>
2020-02-10 18:10:41 +0300

    Hotfix: utils.js linter fixes

--- src/scripts/modules/utils.js
+++ src/scripts/modules/utils.js
@@ -1,75 +1,61 @@
-import PerfectScrollbar from 'perfect-scrollbar';
+import PerfectScrollbar from 'perfect-scrollbar'
 
-import { DOM } from "./configs/path";
+import { DOM } from './configs/path'
 
-//get input value
-export const getInputVal = input => input.value;
+// get input value
+export const getInputVal = input => input.value
 
-//hide elem
-export const hideElem = elem => elem.classList.add('js-hidden');
+// hide elem
+export const hideElem = elem => elem.classList.add('js-hidden')
 
-//show elem
-export const showElem = elem => elem.classList.remove('js-hidden');
+// show elem
+export const showElem = elem => elem.classList.remove('js-hidden')
 
-//perfect scrollbars init
+// perfect scrollbars init
 export const scrollbarsInit = () => {
+  DOM.panels.panelsArr.forEach((elem) => {
+    const ps = new PerfectScrollbar(elem)
+  })
+}
 
-	DOM.panels.panelsArr.forEach((elem) => {
+// clean element inner HTML
+export const cleanElemInner = elem => { elem.innerHTML = '' }
 
-		const ps = new PerfectScrollbar(elem);
+// clean input
+export const cleanInput = input => { input.value = '' }
 
-	});
-
-};
-
-//clean element inner HTML
-export const cleanElemInner = elem => elem.innerHTML = '';
-
-//clean input
-export const cleanInput = input => input.value = '';
-
-//render error messages
+// render error messages
 export const renderErrorMsg = (errorText, renderPlace) => {
+  const error = DOM.errorContainer.cloneNode(true)
 
-	const error = DOM.errorContainer.cloneNode(true);
+  error.classList.remove('js-hidden')
 
-	error.classList.remove('js-hidden');
+  const errorMsg = error.querySelector('p')
 
-	const errorMsg = error.querySelector('p');
+  errorMsg.textContent = errorText
 
-	errorMsg.textContent = errorText;
+  renderPlace.appendChild(error)
+}
 
-	renderPlace.appendChild(error);
-
-};
-
-//find a parent function
+// find a parent function
 export const findParent = (currentElem, parentClass) => {
+  while (!currentElem.classList.contains(parentClass)) {
+    currentElem = currentElem.parentNode
+  }
+  return currentElem
+}
 
-	while(! currentElem.classList.contains(parentClass)) {
-
-		currentElem = currentElem.parentNode;
-
-	}
-
-	return currentElem;
-
-};
-
-//splitting array to pages function
+// splitting array to pages function
 export const pageSplit = (arr, itemsPerPage, currentPage) => {
+  const startInd = itemsPerPage * currentPage - itemsPerPage
 
-	const startInd = itemsPerPage * currentPage - itemsPerPage;
+  const finalInd = itemsPerPage * currentPage
 
-	const finalInd = itemsPerPage * currentPage;
+  return arr.length >= itemsPerPage * currentPage ? arr.slice(startInd, finalInd) : null
+}
 
-	return arr.length >= itemsPerPage * currentPage ? arr.slice(startInd, finalInd) : null;
-
-};
-
-//pagination script
+// pagination script
 export const pagination = (pagination, arr, itemsPerPage, currentPage) => {
-
-	//page splitting
-	return pageSplit(arr, itemsPerPage, currentPage);
-};
+  // page splitting
+  return pageSplit(arr, itemsPerPage, currentPage)
+}
