44082dba65a3964faa4e4d38a8b92062e5d28988
nat-davydova <natti.davydova@gmail.com>
2020-02-10 18:35:15 +0300

    Hotfix: searchView.js linter fixes

--- src/scripts/modules/views/searchView.js
+++ src/scripts/modules/views/searchView.js
@@ -1,68 +1,54 @@
-import { DOM } from '../configs/path';
-import { renderErrorMsg, showElem, pagination, cleanElemInner } from "../utils";
+import { DOM } from '../configs/path'
+import { renderErrorMsg, showElem, pagination, cleanElemInner } from '../utils'
 
-//rendeer search results
+// rendeer search results
 const recipeCard = (id, title, img) => {
+  const card = DOM.recipeCard.card.cloneNode(true)
 
-	const card = DOM.recipeCard.card.cloneNode(true);
+  card.classList.remove('js-hidden')
 
-	card.classList.remove('js-hidden');
+  card.dataset.id = id
 
-	card.dataset.id = id;
+  const recipeTitle = card.querySelector(DOM.recipeCard.title)
 
-	const recipeTitle = card.querySelector(DOM.recipeCard.title);
+  recipeTitle.textContent = title
 
-	recipeTitle.textContent = title;
+  const recipeImg = card.querySelector(DOM.recipeCard.img)
 
-	const recipeImg = card.querySelector(DOM.recipeCard.img);
+  recipeImg.setAttribute('src', img)
 
-	recipeImg.setAttribute('src', img);
+  recipeImg.setAttribute('alt', title)
 
-	recipeImg.setAttribute('alt', title);
-
-	DOM.searchResPanel.results.appendChild(card);
-
-};
+  DOM.searchResPanel.results.appendChild(card)
+}
 
 export const renderSearchResults = (searchArr, errorMsg, itemsPerPage, currentPage) => {
+  if (errorMsg) {
+    renderErrorMsg(errorMsg, DOM.searchResPanel.results)
 
-	if(errorMsg) {
-		renderErrorMsg(errorMsg, DOM.searchResPanel.results);
+    return
+  }
 
-		return;
-	}
+  // clear UI
+  cleanElemInner(DOM.searchResPanel.results)
 
-	//clear UI
-	cleanElemInner(DOM.searchResPanel.results);
+  // render results
+  if (searchArr.length > itemsPerPage) {
+    // pagination init, if there are more elems than displayed on 1 page
+    showElem(document.querySelector(DOM.searchResPanel.pagination))
 
-	//render results
-	if (searchArr.length > itemsPerPage) {
+    const pagedArr = pagination(DOM.searchResPanel.pagination, searchArr, itemsPerPage, currentPage)
 
-		//pagination init, if there are more elems than displayed on 1 page
-		showElem(document.querySelector(DOM.searchResPanel.pagination));
-
-		const pagedArr = pagination(DOM.searchResPanel.pagination, searchArr, itemsPerPage, currentPage);
-
-		if(pagedArr) {
-			pagedArr.forEach(({id, title, image}) => {
-
-				recipeCard(id, title, image);
-
-			});
-		}
-
-	} else if (searchArr.length <= itemsPerPage && searchArr.length > 0) {
-
-		searchArr.forEach(({id, title, image}) => {
-
-			recipeCard(id, title, image);
-
-		});
-
-	} else {
-
-		renderErrorMsg(`Sorry, we can't find any recipes :( Try other keywords!`, DOM.searchResPanel.results);
-
-	}
-
-};
+    if (pagedArr) {
+      pagedArr.forEach(({ id, title, image }) => {
+        recipeCard(id, title, image)
+      })
+    }
+  } else if (searchArr.length <= itemsPerPage && searchArr.length > 0) {
+    searchArr.forEach(({ id, title, image }) => {
+      recipeCard(id, title, image)
+    })
+  } else {
+    renderErrorMsg('Sorry, we can\'t find any recipes :( Try other keywords!', DOM.searchResPanel.results)
+  }
+}
