e1f88438d46d57eaa05feab9dc69a0a60dc81e06
nat-davydova <natti.davydova@gmail.com>
2020-02-10 18:13:27 +0300

    Hotfix: apiKeys.js and path.js linter fixes

--- src/scripts/modules/configs/apiKeys.js
+++ src/scripts/modules/configs/apiKeys.js
@@ -1,1 +1,1 @@
-export const recipeApi = `e7774f8514b14360a25206c47a4bfcb8`;
+export const recipeApi = 'e7774f8514b14360a25206c47a4bfcb8'
--- src/scripts/modules/configs/path.js
+++ src/scripts/modules/configs/path.js
@@ -1,53 +1,53 @@
 export const DOM = {
 
-	search: {
-		btn: '.search-home__btn',
-		field: '.search-home__input',
-	},
+  search: {
+    btn: '.search-home__btn',
+    field: '.search-home__input'
+  },
 
-	panels: {
-		panelsArr: document.querySelectorAll('.panel'),
-		home: document.querySelector('.home-panel'),
-		searchRes: document.querySelector('.search-res-panel'),
-		fullRecipe: document.querySelector('.recipe-panel'),
-	},
+  panels: {
+    panelsArr: document.querySelectorAll('.panel'),
+    home: document.querySelector('.home-panel'),
+    searchRes: document.querySelector('.search-res-panel'),
+    fullRecipe: document.querySelector('.recipe-panel')
+  },
 
-	loaders: {
-		mainLoader: document.querySelector('.preloader'),
-	},
+  loaders: {
+    mainLoader: document.querySelector('.preloader')
+  },
 
-	recipeCard: {
-		card: document.querySelector('.recipe-card'),
-		img: '.recipe-card__img img',
-		title: '.recipe-card__title',
-		moreBtn: '.recipe-card__more',
-	},
+  recipeCard: {
+    card: document.querySelector('.recipe-card'),
+    img: '.recipe-card__img img',
+    title: '.recipe-card__title',
+    moreBtn: '.recipe-card__more'
+  },
 
-	searchResPanel: {
-		results: document.querySelector('.search-results__results'),
-		pagination: '.search-results__pagination',
-	},
+  searchResPanel: {
+    results: document.querySelector('.search-results__results'),
+    pagination: '.search-results__pagination'
+  },
 
-	fullRecipePanel: {
-		content: document.querySelector('.recipe-full__content'),
-		ingred: {
-			card: document.querySelector('.recipe-full__ingred'),
-			content: '.recipe-full__ingred-content p',
-			img: '.recipe-full__ingred-img img',
-		},
-		ingredList: document.querySelector('.recipe-full__ingreds-list'),
-		readyTime: document.querySelector('.ready-in-mins__content'),
-		recipe: document.querySelector('.recipe-full__content-inner'),
-		recipeImg: document.querySelector('.recipe-full__img img'),
-		servings: document.querySelector('.servings__content'),
-		title: document.querySelector('.recipe-full__title'),
-		url: document.querySelector('.recipe-full__more'),
-	},
+  fullRecipePanel: {
+    content: document.querySelector('.recipe-full__content'),
+    ingred: {
+      card: document.querySelector('.recipe-full__ingred'),
+      content: '.recipe-full__ingred-content p',
+      img: '.recipe-full__ingred-img img'
+    },
+    ingredList: document.querySelector('.recipe-full__ingreds-list'),
+    readyTime: document.querySelector('.ready-in-mins__content'),
+    recipe: document.querySelector('.recipe-full__content-inner'),
+    recipeImg: document.querySelector('.recipe-full__img img'),
+    servings: document.querySelector('.servings__content'),
+    title: document.querySelector('.recipe-full__title'),
+    url: document.querySelector('.recipe-full__more')
+  },
 
-	returnBtns: {
-		searchReturn: '.search-results .return-btn',
-		recipeReturn: '.recipe-full .return-btn',
-	},
+  returnBtns: {
+    searchReturn: '.search-results .return-btn',
+    recipeReturn: '.recipe-full .return-btn'
+  },
 
-	errorContainer: document.querySelector('.errorContainer'),
-};
+  errorContainer: document.querySelector('.errorContainer')
+}
