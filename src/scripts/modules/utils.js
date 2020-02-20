c19cb51a2fb6828044ff9e9874990eed2bee8fcd
nat-davydova <natti.davydova@gmail.com>
2020-02-20 19:07:13 +0300

    Hotfix: utils comments improved

--- src/scripts/modules/utils.js
+++ src/scripts/modules/utils.js
@@ -2,23 +2,23 @@ import PerfectScrollbar from 'perfect-scrollbar'
 
 import { PATH } from './configs/path'
 
-// get input value
 export const getInputVal = input => input.value
 
-// hide elem
 export const hideElem = elem => elem.classList.add('js-hidden')
 
-// show elem
 export const showElem = elem => elem.classList.remove('js-hidden')
 
-// toggling elements - one - is hidden, second - is shown
+export const delElem = elem => elem.remove()
+
+export const cleanElemInner = elem => { elem.innerHTML = '' }
+
+export const cleanInput = input => { input.value = '' }
+
+// toggling elements - first - is hidden, second - is shown
 export const toggleElems = (elemToHide, elemToShow) => {
   hideElem(elemToHide)
   showElem(elemToShow)
 }
-
-// delete elem
-export const delElem = elem => elem.remove()
 
 // perfect scrollbars init
 export const scrollbarsInit = () => {
@@ -26,12 +26,6 @@ export const scrollbarsInit = () => {
     const ps = new PerfectScrollbar(elem)
   })
 }
-
-// clean element inner HTML
-export const cleanElemInner = elem => { elem.innerHTML = '' }
-
-// clean input
-export const cleanInput = input => { input.value = '' }
 
 // render error messages
 export const renderErrorMsg = (errorText, renderPlace) => {
@@ -46,7 +40,7 @@ export const renderErrorMsg = (errorText, renderPlace) => {
   renderPlace.appendChild(error)
 }
 
-// find a parent function
+// function which helps to find an element's parent with defined classname
 export const findParent = (currentElem, parentClass) => {
   while (!currentElem.classList.contains(parentClass)) {
     currentElem = currentElem.parentNode
