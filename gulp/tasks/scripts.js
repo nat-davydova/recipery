fdaef38a7ff9488e0c52e4232ae287a6417c7333
nat-davydova <natti.davydova@gmail.com>
2020-02-21 18:08:02 +0300

    Hotfix: bootstrap js removed

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -12,10 +12,7 @@ const del = require('del');
 const jsDev = 'scripts.js'; //file in source folder to compile
 const tempBasename = 'scripts-temp'; //basename for temporary file
 const jsToBundle = `${tempBasename}.js`; //temporary file to bundle with vendors
-const jsVendorsArray = [
-	'node_modules/jquery/dist/jquery.min.js',
-	'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-]; //array of vendor js files from 3rd-party plugins
+const jsVendorsArray = []; //array of vendor js files from 3rd-party plugins
 
 const scriptsDev = function () {
 
